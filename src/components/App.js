53d158165b9dc6d9c20620540aabf5daa190b07d
Marc <marc.ho380@gmail.com>
2018-07-19 13:07:43 -0700

    Added Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+        <div className="App">
+            <Header />
+        </div>
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import '../styles/Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+        <div className="Header">
+            <nav className="HeaderBar navbar navbar-expand-lg navbar-light bg-light">
+                <span className="navbar-brand">SortaBestBuy</span>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">Search Products <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="cart">Cart</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="checkout">Checkout</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+  }
+}
+
+export default Header;
