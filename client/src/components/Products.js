cc6e8ccefb2e24ee7eff0273e5d90b3913d0da45
Marc <marc.ho380@gmail.com>
2018-07-20 20:48:11 -0700

    Added search bar to product search page

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -5,7 +5,13 @@ class Products extends Component {
   render() {
     return (
         <div className="Products">
-            
+            <div className="SearchHeader">
+                <div className="form-group has-feedback has-feedback-left">
+                    <input type="text" className="form-control" id="search-value" placeholder="Search Products" />
+                    <span className="glyphicon glyphicon-search form-control-feedback"></span>
+                    <button type="submit" className="btn btn-primary search-button">Search</button>
+                </div>
+            </div>
         </div>
     );
   }
