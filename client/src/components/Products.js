8758625bb3a158d6cb8e04b882d8357c0c6d8cf8
Marc <marc.ho380@gmail.com>
2018-07-20 22:37:33 -0700

    Added initial blank API call to get products

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
 import '../styles/Products.css';
+import axios from 'axios';
 
 class Products extends Component {
-  render() {
-    return (
-        <div className="Products">
-            <div className="SearchHeader">
-                <div className="form-group has-feedback has-feedback-left">
-                    <input type="text" className="form-control" id="search-value" placeholder="Search Products" />
-                    <span className="glyphicon glyphicon-search form-control-feedback"></span>
-                    <button type="submit" className="btn btn-primary search-button">Search</button>
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchValue: ''
+        }
+    }
+
+    onChangeName(event) {
+        this.setState({
+            searchValue: event.target.value
+        });
+    }
+
+    getProducts() {
+        axios.get('/api/getProducts', {
+            params: {
+              searchValue: this.state.searchValue
+            }
+          })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    render() {
+        return (
+            <div className="Products">
+                <div className="SearchHeader">
+                    <div className="form-group has-feedback has-feedback-left">
+                        <input type="text" className="form-control" id="search-value" placeholder="Search Products" onChange={this.onChangeName.bind(this)} />
+                        <button type="submit" className="btn btn-primary search-button" onClick={this.getProducts.bind(this)}>Search</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default Products;
