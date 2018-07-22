551451b0d5b10cd5d24b94375627f990c3191efe
Marc <marc.ho380@gmail.com>
2018-07-21 22:44:59 -0700

    Added axios to node back end to call Walmart API

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const axios = require('axios');
+const WalmartAPI = '6k2qygre8hb96fd3pdb5keap';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/api/getProducts', (req, res) => {
-    console.log(req.query.searchValue);
-    res.send({ express: 'Hello From Express' });
+    axios.get('http://api.walmartlabs.com/v1/search', {
+        params: {
+            query: req.query.searchValue,
+            numItems: 25,
+            format: 'json',
+            apiKey: WalmartAPI,
+        }
+      })
+        .then(function (response) {
+            res.send(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
